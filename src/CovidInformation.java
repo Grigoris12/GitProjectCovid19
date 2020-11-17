66301392a2d772692afc49c568e571bf3b4a999f
Σίλια <@LAPTOP-2INNR0CN.station>
2020-11-17 14:40:18 +0200

    FinalMenu

--- src/CovidInformation.java
+++ src/CovidInformation.java
@@ -2,5 +2,13 @@
 public class CovidInformation {
 	public static void menu() { 
 	System.out.println("********Μενού Επιλογών********");
+	System.out.println("1.Εκτύπωση των συμπτωμάτων του Covid19");
+	System.out.println("2.Εκτύπωση των οδηγιών για προστασία απο τον Covid19");
+	System.out.println("3.Εκτύπωση σημερινών κρουσμάτων Covid19");
+	System.out.println("4.Εκτύπωση συνολικών κρουσμάτων Covid19");
+	System.out.println("5.Εκτύπωση σημερινών θανάτων από Covid19");
+	System.out.println("6.Εκτύπωση συνολικών θανάτων από Covid19");
+	System.out.println("7.Εκτύπωση των κρουσμάτων Covid19 που βρίσκονται σε ΜΕΘ");
+	System.out.println("8.Εκτύπωση διαθέσιμων κλίνων ΜΕΘ σε κάθε πόλη");
 	}
 }
