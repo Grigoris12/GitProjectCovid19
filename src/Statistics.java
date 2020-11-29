b7749a5ca444ef74a1d1500570c506f32dd641bb
cfragiadakis <72870088+cfragiadakis@users.noreply.github.com>
2020-11-29 17:01:11 +0200

    second commit

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,17 +1,25 @@
 
 public class Statistics {
-	private static int countmale = 0;
-	private static int countfemale = 0;
-	private static int countkids = 0; 
-	private static int countadults = 0;
-	private static int countelders = 0;
+	private static double countmale = 0;
+	private static double totalcount = 0; 
+	private static double countfemale = 0;
+	private static double countkids = 0; 
+	private static double countadults = 0;
+	private static double countelders = 0;
 	public static void Gendercount(boolean infected,String gender){
-		if (infected && gender == "male") {
+		if (infected) {
+			totalcount++;
+			if (gender == "male") {
 			countmale++; 
-		}else if (infected && gender == "female") {
+			}else {
 			countfemale++;
 		}
-		}
+	}
+	}
+	public static void GenderPercentages(){
+		System.out.println(countmale/totalcount *100 + " % of the confirmed cases are male" );
+		System.out.println(countfemale/totalcount *100 + " % of the confirmed cases are female") ;
+	}
 	public static void AgeofPatients(int age) {
 		if (age <= 17) {
 			countkids++;
@@ -20,5 +28,10 @@ public class Statistics {
 		} else {
 			countelders++;
 	}
+	}
+	public static void AgePercentages() {
+		System.out.println(countkids/totalcount *100 + "% of the confirmed cases are kids");
+		System.out.println(countadults/totalcount *100 + "% of the confirmed cases are adults");
+		System.out.println(countelders/totalcount *100 + "% of the confirmed cases are elders"); 
 	}
 }
