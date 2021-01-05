269540e15c08907e55b381dceba19f170d771b9c
Grigoris12 <72793958+Grigoris12@users.noreply.github.com>
2021-01-05 17:20:37 +0200

    1

--- src/MenuLauncher.java
+++ src/MenuLauncher.java
@@ -46,6 +46,7 @@ public class MenuLauncher {
 			System.out.println("1. Sign-Up ");
 			System.out.println("2. Covid Information");
 			System.out.println("3. Log in  (Hospital Manager only)");
+			System.out.println("4. Information about the hospitals");
 			System.out.print("Give [1-3] = ");
 			try {
 				x =  sc.nextInt();
